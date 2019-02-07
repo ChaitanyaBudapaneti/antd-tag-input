ea351ed2e2dcaebef9d8b26c5b09f79ea2a702eb
Chaitanya <chaitanya@alphaclinicalsystems.com>
2019-02-07 20:47:07 +0530

    First commit

---
+++ src/index.js
@@ -0,0 +1,119 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "antd/dist/antd.css";
+import "./index.css";
+import { Form, Input, Button, Tag, Tooltip, Icon } from "antd";
+
+
+export class Tag extends React.Component {
+  
+  static getDerivedStateFromProps(nextProps) {
+    // Should be a controlled component.
+    if ("value" in nextProps) {
+      return {
+        ...(nextProps.value || {})
+      };
+    }
+    return null;
+  }
+
+  constructor(props) {
+    super(props);
+
+    const value = props.value || [];
+    this.state = {
+      tags: value || [],
+      inputVisible: false,
+      inputValue: ""
+    };
+  }
+
+  handleClose = removedTag => {
+    const tags = this.state.tags.filter(tag => tag !== removedTag);
+    console.log(tags);
+    this.setState({ tags });
+    this.triggerChange(tags);
+  };
+
+  showInput = () => {
+    this.setState({ inputVisible: true }, () => this.input.focus());
+  };
+
+  handleInputChange = e => {
+    this.setState({ inputValue: e.target.value });
+  };
+
+  handleInputConfirm = () => {
+    const state = this.state;
+    const inputValue = state.inputValue;
+    let tags = state.tags;
+    if (inputValue && tags.indexOf(inputValue) === -1) {
+      tags = [...tags, inputValue];
+    }
+    console.log(tags);
+    this.setState({
+      tags,
+      inputVisible: false,
+      inputValue: ""
+    });
+    this.triggerChange(tags);
+  };
+
+  saveInputRef = input => (this.input = input);
+
+  triggerChange = changedValue => {
+    // Should provide an event to pass value to Form.
+    const onChange = this.props.onChange;
+    if (onChange) {
+      onChange(changedValue);
+    }
+  };
+
+  render() {
+    const { tags, inputVisible, inputValue } = this.state;
+    console.log(this.props);
+    return (
+      <div>
+        {tags.map((tag, index) => {
+          const isLongTag = tag.length > 20;
+          const tagElem = (
+            <Tag
+              {...this.props}
+              key={tag}
+              afterClose={() => this.handleClose(tag)}
+            >
+              {isLongTag ? `${tag.slice(0, 20)}...` : tag}
+            </Tag>
+          );
+          return isLongTag ? (
+            <Tooltip title={tag} key={tag}>
+              {tagElem}
+            </Tooltip>
+          ) : (
+            tagElem
+          );
+        })}
+        {inputVisible && (
+          <Input
+            ref={this.saveInputRef}
+            type="text"
+            size="small"
+            style={{ width: 78 }}
+            value={inputValue}
+            onChange={this.handleInputChange}
+            onBlur={this.handleInputConfirm}
+            onPressEnter={this.handleInputConfirm}
+          />
+        )}
+        {!inputVisible && (
+          <Tag
+            onClick={this.showInput}
+            style={{ background: "#fff", borderStyle: "dashed" }}
+          >
+            <Icon type="plus" /> New Tag
+          </Tag>
+        )}
+      </div>
+    );
+  }
+}
